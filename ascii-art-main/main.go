08f176c3030fc02c59139712ccc3fcec19517377
hokwach <hezronokwach@gmail.com>
2024-06-21 15:28:48 +0300

    refactor:Refactor ascii art

--- ascii-art-main/main.go
+++ ascii-art-main/main.go
@@ -9,42 +9,7 @@ import (
 
 func main() {
 	// Check if command-line arguments are appropriate, providing usage instructions if not.
-	if len(os.Args) > 3 || len(os.Args) == 1 {
-		fmt.Println("Usage: go run . <string> or go run . <string> <flag>")
-		return
-	}
-	// Validate input string for printable characters.
-	input := os.Args[1]
-	if !asciiart.IsPrintable(input) {
-		fmt.Println("Error: Input string contains non-printable characters")
-		return
-	}
-	// Handle special cases for empty input or newline character.
-	if input == "" {
-		return
-	}
-	if input == "\\n" {
-		fmt.Println()
-		return
-	}
-	// Determine the file path based on the number of command-line arguments and their values.
-	var filePath string
-	if len(os.Args) == 2 {
-		filePath = "standard.txt"
-	}
-	if len(os.Args) == 3 {
-		flag := os.Args[2]
-		if flag == "-shadow" {
-			filePath = "shadow.txt"
-		} else if flag == "-thinkertoy" {
-			filePath = "thinkertoy.txt"
-		} else if flag == "-standard" {
-			filePath = "standard.txt"
-		} else {
-			fmt.Println("Please provide the correct flag, <-shadow> or <-thinkertoy> or <standard>")
-			return
-		}
-	}
+	
 	// Read the ASCII art map from the specified file.
 	characterMap, err := asciiart.CreateMap(filePath)
 	if err != nil {
