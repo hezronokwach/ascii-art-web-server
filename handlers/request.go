9a48df7a933e19bd1d01653bf7383f814cade1a9
bbantu <brianbaraza40@gmail.com>
2024-06-22 16:30:21 +0300

    Add post function for the submit form

--- handlers/request.go
+++ handlers/request.go
@@ -1,15 +1,41 @@
 package handlers
 
 import (
-	//"html/template"
+	"fmt"
 	"net/http"
+	"os"
+
+	//"html/template"
+	asciiart "asciiart/functionFiles"
 )
 
-func Request(writer http.ResponseWriter , reader * http.Request) {
-	temp1:=GetTemplate()
-	if reader.Method != http.MethodGet{
-		http.Error(writer, "405 Invalid request method", http.StatusMethodNotAllowed )
+func Request(writer http.ResponseWriter, reader *http.Request) {
+	temp1 := GetTemplate()
+	if reader.Method != http.MethodGet {
+		http.Error(writer, "405 Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 	temp1.Execute(writer, Data{Success: false})
+}
+
+func Post(writer http.ResponseWriter, reader *http.Request) {
+	temp2 := GetTemplate()
+	if reader.Method != http.MethodPost {
+		http.Error(writer, "405 Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	userInput := reader.FormValue("userinput")
+	banner := reader.FormValue("bannerfile")
+
+	characteMap, err := asciiart.CreateMap(banner)
+	if err != nil {
+		fmt.Printf("Error creating map %s", err)
+		fmt.Println()
+		os.Exit(1)
+	}
+	result := asciiart.DisplayAsciiArt(characteMap, userInput)
+	temp2.Execute(writer, Data{Success:true, Result:result})
+	fmt.Println(userInput)
+	fmt.Println(banner)
+	
 }
