5e8025dc907f79c3e7fcf1e8e9fc997ab89633b1
hokwach <hezronokwach@gmail.com>
2024-07-08 13:43:00 +0300

    refactor: Handle new line in the input Replace \r\n with \n and split with \n

--- functionFiles/displaymap.go
+++ functionFiles/displaymap.go
@@ -8,10 +8,14 @@ import (
 func DisplayAsciiArt(characterMap map[rune][]string, input string) string {
 	// Check if the character map is empty
 	// Split the input string by "\n" to handle newline characters.
-	inputRep := strings.Replace(input, "\r\n", "\n", -1)
-	inputSlice := strings.Split(inputRep, "\n")
+	inputRep := strings.Replace(input, "\r\n", "\\n", -1)
+	inputSlice := strings.Split(inputRep, "\\n")
 	var result strings.Builder
 	for _, value := range inputSlice {
+		if value == "" {
+			result.WriteString("\n") // Ensure empty lines are maintained.
+			continue
+		}
 		for i := 0; i < 8; i++ {
 			for _, char := range value {
 				line, ok := characterMap[char]
