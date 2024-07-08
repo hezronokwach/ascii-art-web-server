4f069f6835e19c22a0a63e030854fbbc6ed04eb4
hokwach <hezronokwach@gmail.com>
2024-07-08 10:49:12 +0300

    refactor: Change the local host port number

--- main.go
+++ main.go
@@ -8,17 +8,15 @@ import (
 	handlers "asciiart/handlers"
 )
 
-
 func main() {
-
-	if len(os.Args) !=1 {
-		fmt.Println("<Usage>", "<go run .>, <go run main.go>")
+	if len(os.Args) != 1 {
+		fmt.Println("<Usage>", "<go run .> or <go run main.go>")
 		os.Exit(1)
 	}
 	http.HandleFunc("/", handlers.Request)
 	http.HandleFunc("/submit", handlers.Post)
-	fmt.Println("Server is starting on http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Println("Server is starting on http://localhost:8090")
+	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
 		fmt.Println("Error starting server")
 		os.Exit(1)
